7c40568b6e44ebb50bef94fd2da7a91ee7e6f8a0
Manu Mateos <manu.mateos@tecnilogica.com>
2017-08-16 16:30:40 +0200

    Initial Commit

---
+++ Elfador/main.swift
@@ -0,0 +1,12 @@
+//
+//  main.swift
+//  Elfador
+//
+//  Created by Manu Mateos on 16/8/17.
+//  Copyright © 2017 Liquid Squad. All rights reserved.
+//
+
+import Foundation
+
+print("Hello, World!")
+
